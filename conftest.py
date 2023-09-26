6c88a2311c2eac0cefa623ac2f7d3926d5dbbd3b
Sapfire65 <cot65@yandex.ru>
2023-09-26 22:38:55 +0300

    update conftest.py

--- conftest.py
+++ conftest.py
@@ -55,7 +55,7 @@ import pytest
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="function", autouse=True)
